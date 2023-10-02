466faa7b007fec57efbb6eca0bfd27fe1f659cf4
Edson Reis <83223751+edsonsreis@users.noreply.github.com>
2023-10-01 21:41:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# projeto-hackweek-devs2blu-backend
+Projeto Hackweek Devs2blu Backend.
+
+Health check disponível no endpoint: http://localhost:8080/actuator/health
---
+++ src/main/java/br/com/devs2blu/hackweekbackend/HackweekBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.devs2blu.hackweekbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackweekBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackweekBackendApplication.class, args);
+	}
+
+}
